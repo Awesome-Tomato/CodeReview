877fe685c605ae3a0d39677670755558fc793211
sukyoung shin <sukyoung.dev@gmail.com>
2022-05-15 23:29:32 +0900

    feat: API 연동

--- countrySearch_js/suKyoung/script.js
+++ countrySearch_js/suKyoung/script.js
@@ -1,44 +1,54 @@
-window.addEventListener('load', init);
+window.addEventListener("load", getDOMElements);
 
-function init() {
-  const searchInput = document.querySelector('.searchInput__text');
-  searchInput.addEventListener('keyup', getKeyWord);
-
-  // const searchResult = document.querySelector('.searchResult');
-  // searchResult.children[0].remove();
-
-  return {searchInput};
-};
+function getDOMElements() {
+  const searchInput = document.querySelector(".searchInput__text");
+  searchInput.addEventListener("keyup", getKeyWord);
+  const searchResult = document.querySelector(".searchResult");
+  return {
+    searchInput, // input field
+    searchResult, // result field
+  };
+}
 
 function getKeyWord(e) {
   const inputValue = e.target.value;
+  if (inputValue[0] === undefined) return;
+
   const firstChar = inputValue[0].toUpperCase();
   const keyWord = inputValue.replace(inputValue[0], firstChar);
 
-  const {searchInput} = init();
+  const { searchInput } = getDOMElements();
   searchInput.value = keyWord;
+  getCountryNamesAndFlag(keyWord);
+}
 
-  console.log(keyWord);
-};
+const BASE_URL = "/api/search";
+const options = { method: "GET" };
+async function getCountryNamesAndFlag(keyWord) {
+  const QUERY_PARAMS = `?keyword=${keyWord}`;
 
-async function getCountries(keyWord) {
-  // keyWord를 토대로 국가정보를 받아옴
-  const URL = `API주소${keyWord}`;
-  const options = {
-    request : GET,
-  };
-  // deleteSearchResult();
   try {
-    await fetch(URL, options)
-    .then(json => console.log(json))
-    .then(response => {
-      console.log(response.data);
-    })
-  } catch(e) {
-    throw Error('oops error');
+    await fetch(BASE_URL + QUERY_PARAMS, options)
+      .then((res) => res.json())
+      .then((res) => {
+        const tempData = [...res];
+
+        const { searchResult } = getDOMElements();
+        searchResult.children[0].remove();
+        const newHTMLUlElement = createHTMLElementsWithData(tempData);
+        searchResult.append(newHTMLUlElement);
+      });
+  } catch (e) {
+    console.error("Failed to get datas.", e);
   }
-};
+}
 
-function deleteSearchResult() {
-
-};
+function createHTMLElementsWithData(dataArray) {
+  const newHTMLUlElement = document.createElement("ul");
+  for (let i = 0; i < dataArray.length; i++) {
+    const newHTMLLiElement = document.createElement("li");
+    newHTMLLiElement.append(dataArray[i]);
+    newHTMLUlElement.append(newHTMLLiElement);
+  }
+  return newHTMLUlElement;
+}
